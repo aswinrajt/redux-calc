885510ce12456bc337d7a31c9d4d31a52ac9b4cd
aswinrajt <aswinraj050@gmail.com>
2024-09-11 01:03:13 +0530

    made responsive

--- src/Calcul.jsx
+++ src/Calcul.jsx
@@ -1,42 +1,56 @@
-import React,{useState} from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { calculation,reset } from './redux/calcuSlice';
-
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculation, reset } from './redux/calcuSlice';
 
 function Calcul() {
-    const calc=useSelector((state)=>state.calcReducer.calculator)
-    console.log(calc);
-    const dispatch=useDispatch()
-    const [inputValue,setInputValue]=useState('')
+    const calc = useSelector((state) => state.calcReducer.calculator);
+    const dispatch = useDispatch();
+    const [inputValue, setInputValue] = useState('');
 
-    const resetInp=()=>{
-        const inputElement=document.getElementById("calc")
-        if(inputElement){
-            inputElement.value=""
+    const resetInp = () => {
+        const inputElement = document.getElementById('calc');
+        if (inputElement) {
+            inputElement.value = '';
         }
-    }
-
-    
-
+    };
 
     return (
         <>
-                            
-
-            <div className='d-flex align-items-center justify-content-center p-5 mt-5' style={{ height: '80vh' }}>
-               
-                <div className='w-50 border border-2 border-dark p-5 ' style={{backgroundColor:'lightblue'}}>
-                <h1 className='text-center text-bolder mb-5 text-dark'>Simple Expression Calculator</h1>
-                    <input type="text" id="calc" onChange={(e)=>{setInputValue(e.target.value)}} className='form-control '  placeholder='enter your expression'/>
-                    <h2 className='text-center mt-2 '>RESULT: {calc}</h2>
-<div className='d-flex justify-content-center'>
-                        <button className='btn btn-success m-3' onClick={()=>{dispatch(calculation(inputValue))}}>Calculate</button>
-                        <button onClick={()=>{dispatch(reset());resetInp()}} className='btn btn-danger m-3' >Clear</button>
-    
-</div>                </div>
-            </div> 
+            <div className="container d-flex align-items-center justify-content-center p-3" style={{ height: '100vh' }}>
+                <div className="col-12 col-md-8 col-lg-6 border border-2 border-dark p-4 rounded" style={{ backgroundColor: 'lightblue' }}>
+                    <h1 className="text-center font-weight-bold mb-4 text-dark">Simple Expression Calculator</h1>
+                    <input
+                        type="text"
+                        id="calc"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        className="form-control mb-3"
+                        placeholder="Enter your expression"
+                        style={{ fontSize: '1.2rem', padding: '10px' }}
+                    />
+                    <h2 className="text-center mt-2">RESULT: {calc}</h2>
+                    <div className="d-flex justify-content-between flex-column flex-sm-row mt-3">
+                        <button
+                            className="btn btn-success mb-2 mb-sm-0"
+                            onClick={() => dispatch(calculation(inputValue))}
+                            style={{ fontSize: '1.1rem', padding: '10px 20px' }}
+                        >
+                            Calculate
+                        </button>
+                        <button
+                            onClick={() => {
+                                dispatch(reset());
+                                resetInp();
+                            }}
+                            className="btn btn-danger"
+                            style={{ fontSize: '1.1rem', padding: '10px 20px' }}
+                        >
+                            Clear
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default Calcul
+export default Calcul;
